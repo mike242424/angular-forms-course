3f59df9a7d226f31b54382e9e3c92dc1617f3c5a
mike242424 <mikebyers24@gmail.com>
2023-10-11 06:12:46 -0400

    alternative form configuration

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -7,9 +7,15 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
   styleUrls: ["./login-reactive.component.css"],
 })
 export class LoginReactiveComponent implements OnInit {
+  email = new FormControl("", [Validators.required, Validators.email]);
+  password = new FormControl("", [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
+
   form = new FormGroup({
-    email: new FormControl("", {validators: [Validators.required, Validators.email]}),
-    password: new FormControl("", {validators: [Validators.required, Validators.minLength(8)]}),
+    email: this.email,
+    password: this.password,
   });
 
   constructor() {}
